9b7d25b29e17eb92c356edd9553a7123f8e9a2b5
adi-13raj <58080329+adi-13raj@users.noreply.github.com>
2021-06-21 23:54:41 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,22 +1,26 @@
-label = "jenkins-node-${UUID.randomUUID().toString()}"
+//label = "jenkins-node-${UUID.randomUUID().toString()}"
+@Library('shared-jenkins-libraries') _
 
 kubeNode(
-  containers: [containerTemplates.docker(name: 'docker', image: 'docker:latest', command: 'cat')],
+  containers: [containerTemplate(name: 'docker', image: 'docker:latest', command: 'cat', ttyEnabled: true)],
   volumes: [
     //hostPathVolume(mountPath: '/home/jenkins/.docker/', hostPath: '/home/jenkins/.docker/'),
     //hostPathVolume(mountPath: '/usr/bin/docker', hostPath: '/usr/bin/docker'),
     hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
   ]
 ) {
-  def imagename = "my-image:${env.BUILD_ID}"
+  def imagename = "my_image:${env.BUILD_ID}"
+  def  my_image
 
   stage('Checkout') {
     checkout(scm)
   }
-
-  stage('Build') {
-      dir('jenkins-k8s-slave') {
-            myimage = docker.build("$imagename")
+container('docker') {
+      stage('Build') {
+       dir('rapid7-test') {
+            my_image = docker.build("$imagename")
+        }
       }
-    }
+}
+}
 }
